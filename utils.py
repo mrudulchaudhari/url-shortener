933174cf2fd07b358bd41fe3ab5ffc6f52689712
Mrudul <mrudulchaudhari0608@gmail.com>
2025-12-12 01:28:50 +0530

    added encode_base62 function

--- utils.py
+++ utils.py
@@ -6,3 +6,16 @@ import base64
 ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 BASE = len(ALPHABET)
 
+
+def encode_base62(num: int) -> str:
+    """Encode an integer into a base62 string."""
+    if num == 0:
+        return ALPHABET[0]
+
+    s = []
+    n = int(num)
+    while n>0:
+        n, rem = divmod(n, BASE)
+        s.append(ALPHABET[rem])
+    return ''.join(reversed(s))
+
